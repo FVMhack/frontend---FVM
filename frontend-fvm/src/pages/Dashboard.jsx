60ce4d6f885eb0df0c134b802cc11cf94018dfbb
Divyanshu Urmaliya <urmaliyadiv04@gmail.com>
2023-02-01 01:26:09 +0530

    Dashboard Testing Table

--- frontend-fvm/src/pages/Dashboard.jsx
+++ frontend-fvm/src/pages/Dashboard.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
 
-export default Dashboard
+  // Usage Tracking
+const usageTrackingData = '123 GB';
+// Storage Utilization
+const storageUtilizationData = '67%';
+// Cost Analysis
+const costAnalysisData = '$12.34';
+  return (   
+    <div className="analytics-container p-4">
+      <h2 className="text-2xl font-bold">Storage Usage Analytics</h2>
+      <div className="analytics-data flex flex-row justify-between p-2">
+        <div className="data-item flex flex-row justify-between p-2">
+          <div className="data-name font-bold">Usage Tracking</div>
+          <div className="data-value">{usageTrackingData}</div>
+        </div>
+        <div className="data-item flex flex-row justify-between p-2">
+          <div className="data-name font-bold">Storage Utilization</div>
+          <div className="data-value">{storageUtilizationData}</div>
+        </div>
+        <div className="data-item flex flex-row justify-between p-2">
+          <div className="data-name font-bold">Cost Analysis</div>
+          <div className="data-value">{costAnalysisData}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
