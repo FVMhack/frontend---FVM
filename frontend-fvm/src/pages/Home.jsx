bbd9ef98a939fd1608654a0a056e2304afb78ada
Divyanshu Urmaliya <urmaliyadiv04@gmail.com>
2023-01-31 11:20:49 +0530

    Added Navbar

--- frontend-fvm/src/pages/Home.jsx
+++ frontend-fvm/src/pages/Home.jsx
@@ -1,18 +1,12 @@
 import React from 'react'
-import Sidebar from '../components/SidebarMenu'
-import DataTable from '../components/DataTable'
+import Navbar from '../components/Navbar'
 
 
 const Home = () => {
   return (
-    <div className="h-screen flex">
-  <div className="w-64 bg-black bg-opacity-75">
-    <Sidebar />
-  </div>
-  <div className="flex-1 overflow-auto">
-    <DataTable />
-  </div>
-</div>
+    <div>
+      <Navbar />
+    </div>
   )
 }
 
